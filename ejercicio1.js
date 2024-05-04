cbb20b8c6d8e1b2c5dfe8a8b857544bb56d11a15
deiybg <deibygorrin@gmail.com>
2024-05-04 02:45:03 +0200

    ejercicio 7
    he terminado 7 ejercicios completos, seguiré en la siguiente sesión por el 8

---
+++ ejercicio1.js
@@ -0,0 +1,20 @@
+let miSuperheroeFavorito;
+miSuperheroeFavorito = "Hulk";
+
+let numeroFavorito;
+numeroFavorito = 50;
+
+let altura;
+altura = 5;
+
+let longitud;
+longitud = 10;
+
+let suma; 
+suma= altura + longitud;
+
+console.log(miSuperheroeFavorito);
+console.log(numeroFavorito);
+console.log(altura);
+console.log(longitud);
+console.log(suma);
---
+++ ejercicio7.js
@@ -0,0 +1,10 @@
+function greaterNumber(numberOne , numberTwo) {
+    if(numberOne > numberTwo){
+        console.log(numberOne);
+    }else{
+        console.log(numberTwo);
+    }
+  }
+
+  greaterNumber(40,10);
+  greaterNumber(-5,10);
